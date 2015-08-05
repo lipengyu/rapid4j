3bbcc5ee5948c21e069d832dd5e35ceffa57342d
Mignet <mignetwee@gmail.com>
2015-08-05 16:56:42 +0800

    md5+base64

---
+++ src/main/java/com/v5ent/rapid4j/core/util/SecurityUtil.java
@@ -0,0 +1,32 @@
+package com.v5ent.rapid4j.core.util;
+
+import java.security.MessageDigest;
+
+public class SecurityUtil {
+	 public static String md5(String s) {
+	        try {
+	            MessageDigest md = MessageDigest.getInstance("md5");
+	            byte b[] = md.digest(s.getBytes());
+	 
+	            // 把二进制串转换成可见字符：Base64
+	            BASE64Encoder base64 = new BASE64Encoder();
+	            return base64.encode(b);
+	        } catch (Exception e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+	 
+	    public static String base64Encode(String s) {
+	        BASE64Encoder base64 = new BASE64Encoder();
+	        return base64.encode(s.getBytes());
+	    }
+	 
+	    public static String base64Decode(String s) {
+	        BASE64Decoder base64 = new BASE64Decoder();
+	        try {
+	            return new String(base64.decodeBuffer(s));
+	        } catch (Exception e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+	}
