30f7d44b3601908cdfd6c7a305d9bf8254f68c8b
Mignet <mignetwee@gmail.com>
2015-09-23 17:33:33 +0800

    delete dummy

--- src/main/java/com/v5ent/rapid4j/core/util/SecurityUtils.java
+++ src/main/java/com/v5ent/rapid4j/core/util/SecurityUtils.java
@@ -2,8 +2,11 @@ package com.v5ent.rapid4j.core.util;
 
 import java.security.MessageDigest;
 
-public class SecurityUtil {
-	/* public static String md5(String s) {
+import sun.misc.BASE64Decoder;  
+import sun.misc.BASE64Encoder;  
+
+public class SecurityUtils {
+	 public static String md5(String s) {
 	        try {
 	            MessageDigest md = MessageDigest.getInstance("md5");
 	            byte b[] = md.digest(s.getBytes());
@@ -14,19 +17,19 @@ public class SecurityUtil {
 	        } catch (Exception e) {
 	            throw new RuntimeException(e);
 	        }
-	    }*/
+	    }
 	 
-	    /*public static String base64Encode(String s) {
+	    public static String base64Encode(String s) {
 	        BASE64Encoder base64 = new BASE64Encoder();
 	        return base64.encode(s.getBytes());
 	    }
 	 
 	    public static String base64Decode(String s) {
-	        BASE64Decoder base64 = new BASE64Decoder();
+	    	BASE64Decoder base64 = new BASE64Decoder();
 	        try {
 	            return new String(base64.decodeBuffer(s));
 	        } catch (Exception e) {
 	            throw new RuntimeException(e);
 	        }
-	    }*/
+	    }
 	}
