edcf03757efa9d110106602ebf321374f3669169
Mignet <mignetwee@gmail.com>
2015-10-08 18:14:59 +0800

    测试dubbo

--- src/main/java/com/v5ent/rapid4j/web/service/impl/DemoServiceImpl.java
+++ src/main/java/com/v5ent/rapid4j/web/service/impl/DemoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.v5ent.rapid4j.web.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.v5ent.rapid4j.web.service.DemoService;
 
+@Service
 public class DemoServiceImpl implements DemoService {
 	 
+	@Override
     public String sayHello(String name) {
         return "Hello " + name;
     }
