9333a078bb1f7b24c2899eb9616d877318ce9290
Mignet <mignetwee@gmail.com>
2016-03-14 15:49:27 +0800

    ok

--- src/main/webapp/app/js/utils.js
+++ src/main/webapp/app/js/utils.js
@@ -1,3 +1,4 @@
+$(function() {
 //重写ajax处理方式
 jQuery(function($){
     // 备份jquery的ajax方法  
@@ -133,3 +134,4 @@ Date.prototype.Format = function (fmt) {
     if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
+});
