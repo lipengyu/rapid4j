2ebfb382f89e7887f6f3b2f0084f2b376e9054a2
Mignet <MignetWee@gmail.com>
2016-03-16 22:45:28 +0800

    fix tips bug

--- src/main/webapp/app/js/utils.js
+++ src/main/webapp/app/js/utils.js
@@ -1,4 +1,3 @@
-$(function() {
 //重写ajax处理方式
 jQuery(function($){
     // 备份jquery的ajax方法  
@@ -134,4 +133,3 @@ Date.prototype.Format = function (fmt) {
     if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
-});
